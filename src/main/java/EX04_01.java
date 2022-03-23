ffdb508106e100aa6988bd65de8299f4c78e0388
yangjunseo <didwnstj134@naver.com>
2022-03-23 10:29:22 +0900

    java exam

---
+++ src/main/java/EX04_01.java
@@ -0,0 +1,20 @@
+public class EX04_01 {
+    public static void main(String[] args) {
+        int a, b = 5, c = 3;
+
+        a = b + c;
+        System.out.printf("%d+%d=%d \n", b, c, a);
+
+        a = b - c;
+        System.out.printf("%d-%d=%d \n", b, c, a);
+
+        a = b * c;
+        System.out.printf("%d *%d = %d \n", b, c, a);
+
+        a = b / c;
+        System.out.printf("%d / %d = %d \n", b, c, a);
+
+        a = b % c;
+        System.out.printf("%d %% %d = %d \n", b, c, a);
+    }
+}
