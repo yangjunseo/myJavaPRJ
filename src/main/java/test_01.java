2cfc0e82b30ea1edab836f8d47377e40bb96082d
yangjunseo <didwnstj134@naver.com>
2022-03-28 22:57:50 +0900

    java exam

---
+++ src/main/java/test_01.java
@@ -0,0 +1,14 @@
+public class test_01 {
+    public static void main(String[] args) {
+
+        int num = 10;
+        int bnum = 0b1010;
+        int onum = 012;
+        int hnum = 0xa;
+
+        System.out.println(num);
+        System.out.println(bnum);
+        System.out.println(onum);
+        System.out.println(hnum);
+    }
+}
