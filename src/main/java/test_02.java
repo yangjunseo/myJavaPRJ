0515687c685f555f18a8650f07cc797afec846f0
yangjunseo <didwnstj134@naver.com>
2022-03-29 21:55:57 +0900

    java exam

---
+++ src/main/java/test_02.java
@@ -0,0 +1,14 @@
+public class test_02 {
+    public static void main(String[] args) {
+        int num;
+        num = 10;
+
+        System.out.println(num);
+
+        int level = 20;
+        System.out.println(level);
+
+        int ns;
+        int numberOFStudent;
+    }
+}
